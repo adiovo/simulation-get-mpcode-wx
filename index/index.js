bd7c0711eb71ed6c65219adbd341aa5d489689bc
dosicker <dosicker@163.com>
2022-03-30 16:40:24 +0800

    init commit by dd...

---
+++ index/index.js
@@ -0,0 +1,51 @@
+const app = getApp()
+
+Page({
+  data: {
+    tips: '',
+    code: ''
+  },
+  onLoad() {
+    this.getAuthLoginCode();
+  },
+  // 获取授权code码
+  getAuthLoginCode() {
+    let _this = this;
+
+    wx.login({
+      success(res) {
+        console.log(res);
+
+        _this.setData({ code: '', tips: '' });
+        wx.setClipboardData({
+          data: res.code,
+          success() {
+            wx.showToast({
+              title: 'Code复制成功！',
+              icon: 'success'
+            })
+          }
+        });
+
+        _this.setData({
+          code: res.code,
+          tips: 'Code复制成功，请 Ctrl + V 使用！（有效期在五分钟内使用，并只限使用一次）'
+        })
+      }
+    })
+  },
+  // 微信登录
+  bindGetUserInfo(e) {
+    this.getAuthLoginCode();
+    // wx.getUserProfile({
+    //   desc: '用于个人中心信息展示及资料完善',
+    //   success(res) {
+    //     console.log(res);
+    //   },
+    //   fail(err) {
+    //     console.error(err);
+    //   },
+    //   complete() { }
+    // })
+  }
+})
